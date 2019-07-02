7fa116ce8e58163e98c65ea2356e2ce0d588ab43
areel <aidanreel@outlook.com>
2019-07-02 10:46:02 +0100

    commit B : Cypress installed, JavaScript tests supported

---
+++ cypress/integration/demo_specs/demo.spec.ts
@@ -0,0 +1,16 @@
+describe("When on landing page", () => {
+  // todo configure Cypress to support TypeScript tests
+  // it("should handle typescript based tests", () => {
+  //   const helperMethod = (urlToTest: string) => {
+  //     cy.visit(urlToTest);
+  //     cy.get("[data-testid=strap-line-text]").should("be.visible");
+  //   };
+
+  //   helperMethod("\\");
+  // });
+
+  it("should be able to see strap line text", () => {
+    cy.visit("/");
+    cy.get("[data-testid=strap-line-text]").should("be.visible");
+  });
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 const App: React.FC = () => {
+  const displayMessage = () => {
+    return (
+      <p data-testid="strap-line-text">
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+    );
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        {displayMessage()}
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -21,6 +27,6 @@ const App: React.FC = () => {
       </header>
     </div>
   );
-}
+};
 
 export default App;
